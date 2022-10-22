63bee5c3680eae6f98b47f33f9ea8a570e3e92ea
AleNardone <98427043+AleNardone@users.noreply.github.com>
2022-10-22 12:49:44 +0200

    Mad Lib Game

---
+++ mad_libs_game.py
@@ -0,0 +1,37 @@
+
+def menu():
+    print("*****   MENU   *****")
+    selection={
+        1: print("1. Doctor")
+        2:
+        3:
+    }
+
+menu()
+
+choice=int(input("\nSelect an option from above: "))
+
+if choice == 1:
+    print("\nWrite the word to complete the story!")
+    one= input("Verb: ")
+    two= input("Adjective: ")
+    three= input("Verb: ")
+    four= input("Part of body: ")
+    five= input("Adverb: ")
+    six= input("Part of body: ")
+    seven= input("Noun: ")
+    eight= input("Verb: ")
+    nine= input("Animal: ")
+    ten= input("Noun: ")
+    eleven= input("Verb: ")
+    twelve= input("Adjective: ")
+    thirdteen= input("Color: ")
+
+    print("\n\nMost doctors agree that bicycle of {} your is a/an {} form of exercise. {} a bicycle enables you to develop your {} muscles as well as {} increase the rate of a {} beat. More {} around the world {} bicycles than drive {}. No matter what kind of {} you are, always {} sure to wear a/an {} helmet. Make sure to have {} reflectors too!".format(one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirdteen))
+
+    
+if choice == 2:
+    
+if choice == 3:
+
+
