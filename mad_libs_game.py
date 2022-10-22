3e885119914020b5d6469c19e86fafced746d6ec
AleNardone <98427043+AleNardone@users.noreply.github.com>
2022-10-22 15:18:06 +0200

    Mad Lib Game

--- mad_libs_game.py
+++ mad_libs_game.py
@@ -1,15 +1,15 @@
+import os
 
 def menu():
     print("*****   MENU   *****")
-    selection={
-        1: print("1. Doctor")
-        2:
-        3:
-    }
+    print("1. Why do be have to take a bicycle?")
+    print("2. Let's have lunch at school")
+    print("3. The story of Tarzan")
 
 menu()
 
 choice=int(input("\nSelect an option from above: "))
+os.system("cls")
 
 if choice == 1:
     print("\nWrite the word to complete the story!")
@@ -27,11 +27,47 @@ if choice == 1:
     twelve= input("Adjective: ")
     thirdteen= input("Color: ")
 
-    print("\n\nMost doctors agree that bicycle of {} your is a/an {} form of exercise. {} a bicycle enables you to develop your {} muscles as well as {} increase the rate of a {} beat. More {} around the world {} bicycles than drive {}. No matter what kind of {} you are, always {} sure to wear a/an {} helmet. Make sure to have {} reflectors too!".format(one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirdteen))
+    os.system("cls")
+    print("\nYour mad lib has been created!")
+    print("\n Most doctors agree that bicycle of {} your is a/an {} form of exercise. {} a bicycle enables you to develop your {} muscles as well as {} increase the rate of a {} beat. More {} around the world {} bicycles than drive {}. No matter what kind of {} you are, always {} sure to wear a/an {} helmet. Make sure to have {} reflectors too!".format(one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirdteen))
+
 
     
 if choice == 2:
-    
+    print("\nWrite the word to complete the story!")
+    one= input("Adjective: ")
+    two= input("Food: ")
+    three= input("Adjective: ")
+    four= input("Noun: ")
+    five= input("Food: ")
+    six= input("Name: ").capitalize()
+    seven= input("Noun: ")
+    eight= input("Verb: ")
+    nine= input("Verb: ")
+    ten= input("Verb: ")
+    eleven= input("Noun: ")
+    twelve= input("Noun: ")
+    thirdteen= input("Name: ").capitalize()
+    fourteen= input("Food: ")
+
+    os.system("cls")
+    print("Your mad lib has been created!")
+    print("It was {} day at school, and {} was super {} for lunch. But when she went outside to eat, a {} stole her {}! {} chased the {} all over school. She {}, {}, and {} through the playground. Then she tripped on her {} and the {} escaped! Luckily, {}'s friends were willing to share their {} with her".format(one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirdteen, fourteen))
+
+
+
 if choice == 3:
+    print("\nWrite the word to complete the story!")
+    one= input("Adjective: ")
+    two= input("Noun: ")
+    three= input("Adjective: ")
+    four= input("Place: ")
+    five= input("Plural noun: ")
+    six= input("Noun: ")
+    seven= input("Funny noise: ")
+    eight= input("Adjective: ")
+    nine= input("Animal in plural: ")
 
-
+    os.system("cls")
+    print("Your mad lib has been created!")
+    print("One of the most characters in fiction is named Tarzan of the {}. Tarzan was raised by a/an {} and lives in the {} jungle in the heart of darkest {}. He spends most of his time eating {} and swinging from tree to {}. Whenever he gets angry, he beats on his chest and says, {}! This is his war cry. His best friend is a/an {} chimpanzee named Cheetah. He is supposed to be able to speak to elephants and {}.".format(one, two, three, four, five, six, seven, eight, nine))
