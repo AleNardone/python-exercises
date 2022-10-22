1faba9607543e07045250945ce8f52ee0889e453
AleNardone <98427043+AleNardone@users.noreply.github.com>
2022-10-22 12:48:14 +0200

    Delete madlibs.py

--- madlibs.py
+++
@@ -1,9 +0,0 @@
-adj = input("Adjective: ")
-verb1 = input("Verb: ")
-verb2 = input("Verb: ")
-famous_person = input("Famous person: ")
-
-madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
-I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}."
-
-print(madlib)
