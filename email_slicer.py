b0ac2fe48bb399fed0c3d8cf30326e811f912262
Ale Nardone <98427043+AleNardone@users.noreply.github.com>
2022-10-25 00:28:10 +0200

    Email Slicer

--- email_slicer.py
+++ email_slicer.py
@@ -1,9 +1,9 @@
 f = open("email_slicer.txt", "w")
 email = input("Enter your email: ").strip()
 username = email[:email.index('@')]
-domain = email[email.index('@')+1:]
-f.write(f"Your username is '{username}' and your domian is '{domain}'")
-print(f"Your username is '{username}' and your domian is '{domain}'")
+domain = email[email.index('@'):]
+f.write(f"Your username is '{username}' and your domain is '{domain}'")
+print(f"Your username is '{username}' and your domain is '{domain}'")
 f.close()
 
 
